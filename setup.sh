ceee5541a3ad533efdbb4ad4037edf86b1965112
Bibash Japrel <60769270+bibashjaprel@users.noreply.github.com>
2025-11-01 22:55:02 +0545

    Add setup script for dotfiles management

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+DOTFILES_DIR="$HOME/.dotfiles"
+
+FILES=(
+  ".bashrc"
+  ".gitconfig"
+  ".inputrc"
+  ".vimrc"
+  ".tmux.conf"
+)
+
+for file in "${FILES[@]}"; do
+  TARGET="$HOME/$file"
+  SOURCE="$DOTFILES_DIR/$file"
+
+  if [ -e "$TARGET" ] || [ -L "$TARGET" ]; then
+    mv "$TARGET" "$TARGET.backup"
+  fi
+
+  ln -s "$SOURCE" "$TARGET"
+done
